e30a43033b6fdf5a70685e23f10f8e13b3676bd4
Mark Noone <manoone@tcd.ie>
2020-07-29 12:02:10 +0100

    Added selection input

--- src/app/components/selectionInput/selectionInput.css.tsx
+++ src/app/components/selectionInput/selectionInput.css.tsx
@@ -1,5 +1,45 @@
 import CSS from 'csstype';
 
 export const BaseStyle: CSS.Properties = {
-    
+    position: 'relative'
+}
+
+export const SelectionValue: CSS.Properties = {
+    backgroundColor: 'white',
+    border: '1px solid #ddd',
+    padding: '5px 10px',
+    width: '260px',
+    borderRadius: '6px',
+    height: '32px',
+    cursor: 'pointer'
+}
+
+export const SelectionPElem: CSS.Properties = {
+    fontFamily: "'Open-sans', sans-serif",
+    fontSize: '14px',
+    fontWeight: 500,
+    color: '#464646'
+}
+
+export const OptionsContainer: CSS.Properties = {
+    boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.15)',
+    backgroundColor: '#ffffff',
+    position: 'absolute',
+    borderRadius: '6px',
+    top: '24px',
+    left: 0,
+    padding: '5px',
+    width: '260px',
+    zIndex: 45
+}
+
+export const OptionList: CSS.Properties = {
+    listStyle: 'none',
+    padding: 0,
+    margin: 0
+}
+
+export const OptionListElem: CSS.Properties = {
+    padding: '5px 0px 5px 15px',
+    cursor: 'pointer'
 }
--- src/app/components/selectionInput/selectionInput.tsx
+++ src/app/components/selectionInput/selectionInput.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
-import {
-    BaseStyle
-} from './selectionInput.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { 
+    BaseStyle, 
+    SelectionValue, 
+    SelectionPElem, 
+    OptionsContainer, 
+    OptionList, 
+    OptionListElem 
+}   from './selectionInput.css';
+import './hover.anim.css';
 
-const SelectionInput: React.FunctionComponent = (props) => {
-    return <></>;
+type SelectionValue = string | number;
+type SelectionProps<T extends SelectionValue> = { 
+    value: T, 
+    options: { 
+        s: string, 
+        value: T
+    }[] 
+
+    onChange: (e: T) => void
+}
+
+function SelectionInput<T extends SelectionValue>(
+    {value, options, onChange}: React.PropsWithChildren<SelectionProps<T>>
+) : JSX.Element {
+    const [isShowing, setIsShowing] = useState(false)
+    return <div style={BaseStyle}>
+        <div style={SelectionValue} onClick={() => setIsShowing(!isShowing)}>
+            <p style={SelectionPElem}>
+                { options.find(val => val.value === value)?.s }
+                <span style={{float: 'right', marginRight: '5px'}}>
+                    <FontAwesomeIcon icon={faCaretDown} />
+                </span>
+            </p>
+        </div>
+        {
+            isShowing?
+            <div style={OptionsContainer}>
+                <ul style={OptionList}> 
+                    { options.map( el => 
+                        <li 
+                          className="selection-input-hover"
+                          style={OptionListElem}
+                          onClick={() => {
+                              onChange(el.value);
+                              setIsShowing(false);
+                          }}>
+                            {el.s} 
+                        </li>
+                    )} 
+                </ul>
+            </div>
+            :null
+        }
+    </div>;
 }
 
 export default SelectionInput;
