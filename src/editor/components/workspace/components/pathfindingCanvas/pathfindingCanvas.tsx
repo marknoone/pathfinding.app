652f28051b095542832756d43661b48763d6fd2d
Mark Noone <manoone@tcd.ie>
2020-07-29 17:06:07 +0100

    Very basic cnavas, still needs animation and context framework

--- src/editor/components/workspace/components/pathfindingCanvas/pathfindingCanvas.tsx
+++ src/editor/components/workspace/components/pathfindingCanvas/pathfindingCanvas.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
+import PureCanvas from './canvas';
 import {
     BaseStyle,
     WorkspaceCanvas
 } from './pathfindingCanvas.css';
 
-const PathfindingCanvas: React.FunctionComponent = (props) => 
-    <div style={BaseStyle}> 
+const PathfindingCanvas: React.FunctionComponent = (props) => {
+    const [ctx, setCtx] = useState<CanvasRenderingContext2D>()
+    const [rAF, setrAF] = useState<number>(0)
+
+    const saveContext = (ctx: CanvasRenderingContext2D) => {
+        setCtx(ctx);
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0, 0, 150, 75);
+    }
+
+    const updateAnimationState = () => {
+        if(!ctx)
+            return
+        
+        ctx.fillStyle = "#FF00FF";
+        ctx.fillRect(0, 0, 150, 75)
+    }
+
+    // useEffect(() => {
+    //     return () => {
+    //         cancelAnimationFrame(rAF);
+    //     }
+    // });
+    
+    return <div style={BaseStyle}>
         <div style={{}}>
-            <canvas style={WorkspaceCanvas}></canvas>
+            <PureCanvas width={300} height={300} contextRef={saveContext}/>
         </div>
     </div>;
+}
+    
 
 export default PathfindingCanvas;
